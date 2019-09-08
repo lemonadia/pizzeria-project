77225a9cf649cb69e0ebdde9d25634fffc07fda2
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2019-09-08 17:57:00 +0200

    Add activatePage function

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import {Product} from './components/Product.js';
 import {Cart} from './components/Cart.js';
-import {select, settings} from './settings.js';
+import {select, settings, classNames} from './settings.js';
 
   const app = {
     initMenu: function(){
@@ -61,10 +61,48 @@ import {select, settings} from './settings.js';
     //  console.log('settings:', settings);
     //  console.log('templates:', templates);
 
+      thisApp.initPages();
       thisApp.initData();
+      thisApp.initCart();
     },
+
+    initPages: function () {
+      const thisApp = this;
+
+      thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
+      thisApp.navLinks = Array.from(document.querySelector(select.nav.links));
+
+      thisApp.activatePage(thisApp.pages[0].id);
+
+      for(let link of thisApp.navLinks){
+        link.addEventListener('click', function(event) {
+          const clickedElement = this;
+          event.preventDefault();
+
+          /* TODO: get page id from href */
+
+          const href = clickedElement.getAttribute('href');
+          const pageId = href.replace('#', '');
+
+          /* TODO activate page */
+          thisApp.activatePage(pageId);
+
+        });
+      }
+    },
+
+    activatePage: function(pageId){
+
+      const  thisApp = this;
+
+      for(let link of thisApp.navLinks){
+        link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId); // sprawdzamy, czy href danego linka jest równy id strony, którą mamy aktywować (z prefiksem w postaci #).
+      }
+      for(let link of thisApp.pages){
+        link.classList.toggle(classNames.nav.active, link.getAttribute('id') == pageId);
+      }
+    }
 
   };
 
 app.init();
- app.initCart();
