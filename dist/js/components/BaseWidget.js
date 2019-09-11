5f75458e287e48f6107178529b71780fd002dc7e
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2019-09-11 12:14:27 +0200

    Update code

--- dist/js/components/BaseWidget.js
+++ dist/js/components/BaseWidget.js
@@ -1,14 +1,11 @@
-
-
-
-export class BaseWidget{
+export class BaseWidget {
   constructor(wrapperElement, initialValue) {
-
     const thisWidget = this;
+
+    thisWidget.dom = {};
 
     thisWidget.dom.wrapper = wrapperElement;
     thisWidget.correctValue = initialValue; //ustawienie początkowej wartości
-
   }
 
   get value() {
@@ -16,18 +13,20 @@ export class BaseWidget{
 
     return thisWidget.correctValue;
   }
+//Setter zostanie wywołany, kiedy właściwości value zostanie przypisana jakaś wartość – np. za pomocą widget.value = 5;.
+//Przypisywana wartość – w tym przykładzie 5 – zostanie przekazana setterowi jako pierwszy argument, który nazwaliśmy assignedValue.
 
   set value(assignedValue) {
     const thisWidget = this;
 
     const newValue = thisWidget.parseValue(assignedValue);
-
     // blok if sprawdza, czy warsoć jest inna od dotychczasowej
 
     if (newValue != thisWidget.correctValue && thisWidget.isValid(newValue)) {
-      thisWidget.correntValue = newValue; //jeżeli warunek jest spełniony nowa wartość zapisywana jest w  correctValue
-      thisWidget.announce(); // i uruchamiana jest metoda announce, która wywała event o zmianie warsoći
+      thisWidget.correctValue = newValue; //jeżeli warunek jest spełniony nowa wartość zapisywana jest w  correctValue
+      thisWidget.announce(); //  i uruchamiana jest metoda announce, która wywała event o zmianie warsoći
     }
+
     thisWidget.renderValue(); // wywołujemy metodę renderującą (wyświetlającą) wartość naszego widgetu
   }
 
@@ -39,12 +38,20 @@ export class BaseWidget{
     return !isNaN(newValue);
   }
 
+  renderValue() {
+    const thisWidget = this;
+
+    console.log('widget value:', thisWidget.value);
+  }
+
   announce() {
     const thisWidget = this;
 
     const event = new CustomEvent('updated', {
       bubbles: true
     });
+
     thisWidget.dom.wrapper.dispatchEvent(event);
   }
+
 }
--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -2,68 +2,60 @@ import {
   select,
   settings
 } from '../settings.js';
+import {
+  BaseWidget
+} from './BaseWidget.js';
 
-import {BaseWidget} from './BaseWidget.js';
-
-export class AmountWidget extends BaseWidget{
-  constructor(element) {
-    super(element, settings.amountWidget.defaultValue);
+export class AmountWidget extends BaseWidget {
+  constructor(wrapper) {
+    super(wrapper, settings.amountWidget.defaultValue);
 
     const thisWidget = this;
 
-    thisWidget.getElements(element);
-    thisWidget.value = settings.amountWidget.defaultValue;
-    thisWidget.setValue(thisWidget.input.value);
+    thisWidget.getElements();
     thisWidget.initActions();
 
     //    console.log('AmountWidget: ', thisWidget);
     //    console.log('constructor arguments: ', element);
   }
 
-  getElements(element) {
+  getElements() {
     const thisWidget = this;
 
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
 
-
-  setValue(value) { //ustawiamy wartość tego widgetu
-
-    const thisWidget = this;
-
-    const newValue = parseInt(value); //potrzebna do walidacje i sprawdzenia czy wartość tej stałej jest poprawna i mieści się w dopuszczalnym zakresie
-
-    /* TODO: Add Validation*/
-
-    if (newValue !== thisWidget.value && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax) {
-      thisWidget.value = newValue;
-      thisWidget.announce();
-    }
-
-    thisWidget.input.value = thisWidget.value; // dzięki temu nowa wartość wyświetli się na stronie
-  }
 
   initActions() {
 
     const thisWidget = this;
 
-    thisWidget.input.addEventListener('change', function() {
-      thisWidget.setValue(thisWidget.input.value);
+    thisWidget.dom.input.addEventListener('change', function() {
+      thisWidget.value = thisWidget.dom.input.value;
     });
 
-    thisWidget.linkDecrease.addEventListener('click', function(event) {
+    thisWidget.dom.linkDecrease.addEventListener('click', function(event) {
       event.preventDefault();
-      thisWidget.setValue(thisWidget.value - 1);
+      thisWidget.value--;
     });
 
-    thisWidget.linkIncrease.addEventListener('click', function(event) {
+    thisWidget.dom.linkIncrease.addEventListener('click', function(event) {
       event.preventDefault();
-      thisWidget.setValue(thisWidget.value + 1);
-
-
+      thisWidget.value++;
     });
   }
+// sprawdza nie tylko, czy ustawiana wartość value jest liczbą, 
+//ale również czy mieści się w przedziale zdefiniowanym w settings.amountWidget.
+  isValid(newValue) {
+    return !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax;
+  }
+
+  renderValue() {
+    const thisWidget = this;
+
+    thisWidget.dom.input.value = thisWidget.value;
+  }
+
 }
